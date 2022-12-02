7cfb899ab3e8f88ed71568cea90d69f483b1f667
andrewjwhite99 <andrewj.white@bjss.com>
2022-12-02 09:49:50 +0000

    did day2

---
+++ day2/puzzle1/solution.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+f = open("puzzleInput.csv", "r")
+
+data = f.read().split("\n")
+
+
+def rps_score(round):
+    round_score = 0
+    if round[1] == "X":
+        round_score += 1
+        if round[0] == "A":
+            round_score += 3
+        elif round[0] == "C":
+            round_score += 6
+    elif round[1] == "Y":
+        round_score += 2
+        if round[0] == "A":
+            round_score += 6
+        elif round[0] == "B":
+            round_score += 3
+    elif round[1] == "Z":
+        round_score += 3
+        if round[0] == "B":
+            round_score += 6
+        elif round[0] == "C":
+            round_score += 3
+    return round_score
+
+
+rounds = []
+
+for round in data:
+    rounds.append(round.split(" "))
+
+print(rounds)
+
+score = 0
+for round in rounds:
+    score += rps_score(round)
+
+print(score)
